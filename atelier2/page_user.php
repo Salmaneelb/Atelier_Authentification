866574275380b87b6e8d098ac14b93ed7fdbd5a6
Salmaneelb <salmane.elboubakkari.seb@gmail.com>
2025-11-05 10:44:48 +0100

    Update page_user.php

--- atelier2/page_user.php
+++ atelier2/page_user.php
@@ -1,1 +1,17 @@
+<?php 
 
+session_start(); 
+
+if (isset($_COOKIE['authToken'], $_SESSION['authToken']) &&
+    $_COOKIE['authToken'] === $_SESSION['authToken'] ) {
+    header('Location: page_admin.php');
+    exit();
+}
+
+?>
+
+
+<html>
+
+  <h1> Bienvue sur la page USER</h1>
+</html>
