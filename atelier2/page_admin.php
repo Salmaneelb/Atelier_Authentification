63d5c6e09034c8dc3fe6b4d5815821610b7d3a92
Salmaneelb <salmane.elboubakkari.seb@gmail.com>
2025-11-05 11:05:25 +0100

    Update page_admin.php

--- atelier2/page_admin.php
+++ atelier2/page_admin.php
@@ -1,16 +1,16 @@
 <?php
-// Démarrer la session
 session_start();
 
-// Vérifier si l'utilisateur est bien en possession d'un cookie valide
-// Dans le cas contraire il sera redirigé vers la page d'accueil de connexion
-if (!isset($_COOKIE['authToken'], $_SESSION['authToken']) ||
-    $_COOKIE['authToken'] !== $_SESSION['authToken']) {
+// Vérifie cookie + session + rôle admin
+if (
+    !isset($_COOKIE['authToken'], $_SESSION['authToken'], $_SESSION['role']) ||
+    $_COOKIE['authToken'] !== $_SESSION['authToken'] ||
+    $_SESSION['role'] !== 'admin'
+) {
     header('Location: index.php');
     exit();
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
